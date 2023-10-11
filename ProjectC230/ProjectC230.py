4eded74dfb85866bc62164d60a8991e78325fe29
Emirlord13@gmail.com <Emirlord13@gmail.com>
2023-10-11 12:47:50 -0400

    test1

--- ProjectC230/ProjectC230.py
+++ ProjectC230/ProjectC230.py
@@ -6,3 +6,4 @@ print('Shape of the given dataset: ', dataset.shape)
 print('Count of columns: ', len(dataset.columns))
 print('Name of the used parameters: ', dataset.columns)
 print('Display the empty row data: ', dataset.loc[:, dataset.isna().any()])
+#Test
